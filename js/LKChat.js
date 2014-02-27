e04b9f40ad207af3211c0eaaaea6ccff206ca521
Dan DuLeone <dantothelz@gmail.com>
2014-02-27 12:24:04 -0500

    Adding connection timer and messaging.

--- js/LKChat.js
+++ js/LKChat.js
@@ -17,7 +17,7 @@ Number.prototype.pad = function (len) {
 
 app.controller('ChatClient', function ($scope, storage) {
   var t = new Date();
-  $scope.chatLog = "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Connecting to chat server...\n";
+  $scope.chatLog += "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Connecting to chat server...\n";
 
   $scope.appendMessage = function (msg, apply) {
     $scope.chatLog += "[" + msg.time + "] <" + msg.nick + "> " + msg.text + "\n";
@@ -50,16 +50,16 @@ app.controller('ChatClient', function ($scope, storage) {
     },
     onclose: function(e) {
       console.log("Connection lost!");
-      $scope.chatLog = "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Disconnected from chat server...\n";
+      $scope.chatLog += "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Disconnected from chat server...\n";
       conn = new WebSocket(SERVER.getURI());
-      $scope.chatLog = "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Reconnecting to chat server...\n";
+      $scope.chatLog += "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Reconnecting to chat server...\n";
       var conn_check = setInterval(
         function () {
           if (conn.readyState == 1) {
             conn.onopen = this.onopen;
             conn.onmessage = this.onmessage;
             conn.onclose = this.onclose;
-            $scope.chatLog = "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Now entering chatroom...\n";
+            $scope.chatLog += "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Now entering chatroom...\n";
             clearInterval(conn_check);
           }
         },
