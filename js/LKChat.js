465945d1bb1f9fe8e8fe86d475a0c2fa15453031
Dan DuLeone <dantothelz@gmail.com>
2014-02-27 17:59:39 -0500

    Refactored the client a bit.

--- js/LKChat.js
+++ js/LKChat.js
@@ -3,96 +3,104 @@ app = angular.module("LKChat", ['angularLocalStorage']);
 var SERVER = {
   _address: "www.duleone.com",
   _port: "8000",
-  getURI: function () {
+  getURI: function() {
     return "ws://" + this._address + ":" + this._port;
   }
 };
 
-
 //http://stackoverflow.com/questions/3313875/javascript-date-ensure-getminutes-gethours-getseconds-puts-0-in-front-i
-function pad(n) { return ("0" + n).slice(-2); }
-Number.prototype.pad = function (len) {
-    return (new Array(len+1).join("0") + this).slice(-len);
+function pad(n) {
+  return ("0" + n).slice(-2);
+}
+Number.prototype.pad = function(len) {
+  return (new Array(len + 1).join("0") + this).slice(-len);
 }
 
-app.controller('ChatClient', function ($scope, storage) {
-  var t = new Date();
-  $scope.chatLog = "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Connecting to chat server...\n";
 
-  $scope.appendMessage = function (msg, apply) {
-    $scope.chatLog += "[" + msg.time + "] <" + msg.nick + "> " + msg.text + "\n";
-    if ( (typeof apply != 'undefiled') && (apply == 'true') ) {
-      $scope.$apply();
+
+function ChatAppFactory($scope) {
+
+  var appObj = {
+    log: function(msg) {
+      console.log(msg);
     }
-    
   };
+
+  var getTimeStamp = function() {
+    var t = new Date();
+    return t.getHours().pad(2) + ":" + t.getMinutes().pad(2);
+  };
+
+  appObj.formatBufferEntry = function(msg, handle) {
+    if (typeof handle != 'undefined') {
+      return "[" + getTimeStamp() + "] <" + handle + "> " + msg + "\n";
+    } else {
+      return "[" + getTimeStamp() + "] " + msg + "\n";
+    }
+  };
+
+  appObj.appendBufferEntry = function(entry, handle) {
+    if (typeof handle != 'undefined') {
+      $scope.chatLog += appObj.formatBufferEntry(entry, handle);
+    } else {
+      $scope.chatLog += appObj.formatBufferEntry(entry);
+    }
+  };
+
+  appObj.attachHandlers = function() {
+    appObj.conn.onopen = function(e) {
+      appObj.log("Connection established!");
+    };
+
+    appObj.conn.onmessage = function(e) {
+      text = e.data;
+      try {
+        var msg = JSON.parse(text);
+        $scope.$apply(appObj.appendBufferEntry(msg.text, msg.nick));
+      } catch (err) {
+        // Invalid Message.
+        appObj.log("Invalid message received! [" + text + "]");
+        appObj.log("Error! " + err);
+      }
+    };
+
+    appObj.conn.onclose = function(e) {
+      appObj.log("Connection lost!");
+      $scope.$apply(appObj.appendBufferEntry("Disconnected from chat server!"));
+      appObj.createConnection(this);
+    };
+  };
+
+  appObj.createConnection = function() {
+    appObj.conn = new WebSocket(SERVER.getURI());
+    appObj.appendBufferEntry("Connecting to chat server...");
+    var conn_check = setInterval(
+      function() {
+        appObj.log("Attempting to connect...");
+        if (appObj.conn.readyState == 1) {
+          appObj.attachHandlers();
+          $scope.$apply(appObj.appendBufferEntry("Now entering chatroom!"));
+          appObj.log("Connection established.");
+          clearInterval(conn_check);
+        }
+      },
+      1000
+    );
+  };
+
+  return appObj;
+};
+
+
+app.controller('ChatClient', function($scope, storage) {
+  $scope.chatLog = "";
+  var ChatApp = ChatAppFactory($scope);
 
   storage.bind($scope, 'chatHandle');
 
-  var conn = new WebSocket(SERVER.getURI());
+  ChatApp.createConnection();
 
-  var connHandler = {
-    onopen: function(e) {
-      console.log("Connection established!");
-    }, 
-    onmessage: function(e) {
-      text = e.data;
-      console.log('INCOMING!');
-      console.log(text);
-      try {
-        var msg = JSON.parse(text);
-        $scope.appendMessage(msg, 'true');
-      } catch (err) {
-        console.log("Invalid message received! " + text);
-        console.log("Error! " + err);
-        // Invalid Message.
-      }
-    },
-    onclose: function(e) {
-      console.log("Connection lost!");
-      var t = new Date();
-      $scope.chatLog += "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Disconnected from chat server...\n";
-      $scope.$apply();
-      conn = new WebSocket(SERVER.getURI());
-      $scope.chatLog += "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Reconnecting to chat server...\n";
-      $scope.$apply();
-      var conn_check = setInterval(
-        function () {
-          console.log("Attempting to reconnect...");
-          if (conn.readyState == 1) {
-            conn.onopen = this.onopen;
-            conn.onmessage = this.onmessage;
-            conn.onclose = this.onclose;
-            $scope.chatLog += "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Now entering chatroom!\n";
-            $scope.$apply();
-            console.log("Connection established.");
-            clearInterval(conn_check);
-          }
-        },
-        1000
-      );
-    }
-  };
-
-  var conn_check = setInterval(
-    function () {
-      console.log("Attempting to connect...");
-      if (conn.readyState == 1) {
-        var t = new Date();
-        conn.onopen = connHandler.onopen;
-        conn.onmessage = connHandler.onmessage;
-        conn.onclose = connHandler.onclose;
-        $scope.chatLog += "[" + t.getHours().pad(2) + ":" + t.getMinutes().pad(2) + "] Now entering chatroom!\n";
-        $scope.$apply();
-        console.log("Connection established.");
-        clearInterval(conn_check);
-      }
-    },
-    1000
-  );
-
-
-  var MsgPacket = function () {
+  var MsgPacket = function() {
     var t = new Date();
     return {
       "nick": $scope.chatHandle,
@@ -102,13 +110,13 @@ app.controller('ChatClient', function ($scope, storage) {
   }
 
 
-  $scope.sendMessage = function () {
+  $scope.sendMessage = function() {
     msg = new MsgPacket();
 
-    console.log('send message: ' + $scope.chatMsg);
+    ChatApp.log('send message: ' + $scope.chatMsg);
     $scope.chatMsg = "";
-    $scope.appendMessage(msg);
-    conn.send(JSON.stringify(msg));
+    ChatApp.appendBufferEntry(msg.text, msg.nick);
+    ChatApp.conn.send(JSON.stringify(msg));
   }
 });
 
